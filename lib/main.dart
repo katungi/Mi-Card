dd0a20f31fb3aca1c5fdf35fda357ebda0ab12fd
katungi <dankatdennis@gmail.com>
2019-09-09 08:04:12 +0300

    added images and new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  MyApp()
   );
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.pinkAccent,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/dino.jpg'),    
+              ),
+              Text(
+                'Daniel Katungi',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
